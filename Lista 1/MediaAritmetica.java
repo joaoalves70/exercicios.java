9ad710ae2761311ccb26c034392144c3e0990f3e
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-10-27 11:25:37 -0300

    Update and rename 1.java to MediaAritmetica.java

--- Lista 1/MediaAritmetica.java
+++ Lista 1/MediaAritmetica.java
@@ -4,7 +4,7 @@
  *
  *************************************************************************/
 import java.util.Scanner;
-public class Main
+public class MediaAritmetica
 {
   public static void main (String[]args)
   {
