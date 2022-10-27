5bbfa51161b641b72e940191445f7f2e90bc9f24
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-10-27 11:21:13 -0300

    Create 1.java

---
+++ Lista 1/1.java
@@ -0,0 +1,28 @@
+/*************************************************************************
+ *  1)
+ *  Leia 3 números inteiros e imprima a média aritmética entre os números
+ *
+ *************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+  public static void main (String[]args)
+  {
+     Scanner ler = new Scanner(System.in);
+
+       double a, b, c, media;
+
+       System.out.printf("Digite o primeiro valor: ");
+       a = ler.nextInt();
+
+       System.out.printf("Digite o segundo valor: ");
+       b = ler.nextInt();
+
+       System.out.printf("Digite o terceiro valor: ");
+       c = ler.nextInt();
+
+       media = (a+b+c)/3;
+
+       System.out.println("Certo, a média artimética é: "+media); 
+  }
+}
