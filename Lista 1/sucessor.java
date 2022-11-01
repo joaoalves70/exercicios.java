7371626f0fb529c8fd6f56789a57f0c5b63cfdda
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-01 14:48:20 -0300

    Create sucessor.java

---
+++ Lista 1/sucessor.java
@@ -0,0 +1,29 @@
+/*************************************************************************
+ *  11) 
+ *  Faça um programa que receba um número inteiro e que imprima o:
+ *  antecessor, sucessor, o dobro e a metade do número informado
+ *************************************************************************/
+ 
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int x, antecessor, sucessor, metade, dobro;
+	    Scanner ler = new Scanner (System.in);
+	    
+	    System.out.println("Digite um número: ");
+	    x = ler.nextInt();
+	    
+	    antecessor = x-1;
+	    sucessor = x+1;
+	    metade = x/2;
+	    dobro = x*2;
+	    
+	    System.out.println("Seu antecessor é: "+antecessor);
+	    System.out.println("Seu sucessor é: "+sucessor);
+	    System.out.println("Seu metade é: "+metade);
+	    System.out.println("Seu dobro é: "+dobro);
+	    
+	}
+}
