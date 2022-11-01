25ab43c8ada5bc3d8fa9b1b6f217e8454e929b62
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-01 14:48:46 -0300

    Update sucessor.java

--- Lista 1/sucessor.java
+++ Lista 1/sucessor.java
@@ -5,7 +5,7 @@
  *************************************************************************/
  
 import java.util.Scanner;
-public class Main
+public class sucessor
 {
 	public static void main(String[] args) {
 	    
