75372c5a688785d6079aa4f838d2ec299180cede
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-11-01 14:52:07 -0300

    Update operacoesMat.java

--- Lista 1/operacoesMat.java
+++ Lista 1/operacoesMat.java
@@ -5,7 +5,7 @@
  *********************************************************************/
  
  import java.util.Scanner;
-public class Opera
+public class OperacoesMat
 {
 	public static void main(String[] args) {
 	    
