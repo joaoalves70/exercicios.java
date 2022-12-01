d266e7b0ea788a185233c5336ec22464216315e0
João Alves <81262157+joaoalves70@users.noreply.github.com>
2022-12-01 14:26:57 -0300

    Update Imc.java

--- Lista 2/Imc.java
+++ Lista 2/Imc.java
@@ -6,7 +6,7 @@
 *  e o quadrado da Altura (ALTURA – em m) do indivíduo. Ou seja, IMC= PESO/ALTURA2
 ***********************************************************************************/
 import java.util.Scanner; 
-public class Main{ 
+public class Imc{ 
 
     public static void main(String[] args) {
     Scanner ler = new Scanner(System.in);   
